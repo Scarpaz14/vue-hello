4d19e1fac9c52ef9ba7302fb2dc8bb233788f4d7
Scarpaz14 <scarparofrancesco14@gmail.com>
2022-05-19 15:36:42 +0200

    aggiunta immagine

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const app= new Vue(
     {
         el: `#root`,
         data:{
-            message:"ciaooo"
+            message:"ciaooo",
+            image:"https://yt3.ggpht.com/ytc/AKedOLTO2XVhtyMr24Dnz6QJ-Lsj_05XHim-qMoF6PRc=s900-c-k-c0x00ffffff-no-rj"
         }
     }
 );
